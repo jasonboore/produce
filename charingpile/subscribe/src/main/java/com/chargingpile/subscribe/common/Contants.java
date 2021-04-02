8b7f8b556e38affa4fec95f3e608014328413337
jasonboore <46415741+jasonboore@users.noreply.github.com>
2021-04-02 11:12:56 +0800

    Delete charingpile directory

--- charingpile/subscribe/src/main/java/com/chargingpile/subscribe/common/Contants.java
+++
@@ -1,9 +0,0 @@
-package com.chargingpile.subscribe.common;
-
-/**
- * 系统常量  发布(Publish)/订阅(Subscribe)
- */
-public   class Contants {
-    public  static  final  String MQTT_SUBSCRIBE_CHANNEL="mqttSubscribeChannel";
-
-}
--- charingpile/wxapp/src/main/java/com/chargingpile/wxapp/constant/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.chargingpile.wxapp.constant;
-
-import java.util.HashMap;
-
-public class Constants {
-    public static HashMap<String, String> CODE_AREA = new HashMap<>();
-}
